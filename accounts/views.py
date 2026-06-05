fbdc03e50361b375ca5c128a19bb1b6cdc9cb1fb
Varshamadhavan1403 <varshamadhavan1622@gmail.com>
2026-06-05 18:19:55 +0530

    Implement JWT authentication and protected profile api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+
+class ProfileView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response({
+            'username': request.user.username,
+            'email': request.user.email,
+        })
 # Create your views here.
