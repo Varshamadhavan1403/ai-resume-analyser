73737daadbe211b9eba9426f92ba4fd262395a34
Varshamadhavan1403 <varshamadhavan1622@gmail.com>
2026-06-08 14:55:38 +0530

    create resumes app

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'accounts',
+    'resumes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
