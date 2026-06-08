bf750db6cdc1e1754e97b8c489e7b625a3dcf8fe
Varshamadhavan1403 <varshamadhavan1622@gmail.com>
2026-06-08 15:00:30 +0530

    create resume model

--- resumes/models.py
+++ resumes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class Resume(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='resumes')
+    file = models.FileField(upload_to='resumes/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
