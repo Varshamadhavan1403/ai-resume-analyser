bff2b92e99b039aef3ecc2eae2730e60da1c5e19
Varshamadhavan1403 <varshamadhavan1622@gmail.com>
2026-06-10 15:23:50 +0530

    Add extracted_skills field to resume model

--- resumes/models.py
+++ resumes/models.py
@@ -8,6 +8,6 @@ class Resume(models.Model):
     file = models.FileField(upload_to='resumes/')
     parsed_text = models.TextField(blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
+    extracted_skills = models.JSONField(default=list, blank=True)
     def __str__(self):
         return self.file.name
