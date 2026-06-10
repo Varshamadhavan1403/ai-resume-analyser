7b08b971bb67cee6c52e8ca2b4bb3af8814f99ec
Varshamadhavan1403 <varshamadhavan1622@gmail.com>
2026-06-10 15:50:49 +0530

    Add resume detail, list apis and resume skills api end point

--- resumes/urls.py
+++ resumes/urls.py
@@ -1,5 +1,9 @@
-from .views import ResumeUploadView
+from .views import ResumeUploadView, ResumeDetailView, ResumeListView, ResumeSkillsView
 from django.urls import path
 
 urlpatterns = [
-    path('upload/', ResumeUploadView.as_view(), name='resume-upload'),]
+    path('upload/', ResumeUploadView.as_view(), name='resume-upload'),
+    path('<int:pk>/', ResumeDetailView.as_view(), name='resume-detail'),
+    path('', ResumeListView.as_view(), name='resume-list'),
+    path('<int:pk>/skills/', ResumeSkillsView.as_view(), name='resume-skills'),
+]
--- resumes/views.py
+++ resumes/views.py
@@ -3,7 +3,10 @@ from rest_framework import generics, permissions
 from .models import Resume
 from .serializers import ResumeSerializer
 from . services import ResumeParser
-
+from .skill_extractor import SkillExtractor
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -13,6 +16,36 @@ class ResumeUploadView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         resume = serializer.save(user=self.request.user)
-        extracted_text = ResumeParser.parse(resume.file.path)
-        resume.parsed_text = extracted_text
+        # extracted_text = ResumeParser.parse(resume.file.path)
+        # resume.parsed_text = extracted_text
+        # resume.save()
+        parsed_text = ResumeParser.parse(resume.file.path)
+        skills = SkillExtractor.extract(parsed_text)
+        resume.parsed_text = parsed_text
+        resume.extracted_skills = skills
         resume.save()
+
+
+class ResumeDetailView(generics.RetrieveAPIView):
+    serializer_class = ResumeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Resume.objects.filter(user=self.request.user)
+    
+
+class ResumeListView(generics.ListAPIView):
+    serializer_class = ResumeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Resume.objects.filter(user=self.request.user)
+    
+
+class ResumeSkillsView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, pk):
+        resume = get_object_or_404(Resume, user=request.user, pk=pk)
+        return Response ({ "skills": resume.extracted_skills , "resume_id": resume.id})
+        
